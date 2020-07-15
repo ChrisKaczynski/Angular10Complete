99213af4156ce7f7b6d5e1393616b3a2b41bdceb
ChrisKaczynski <krzysztof.kaczynski@vizlib.com>
2020-07-15 14:05:09 +0200

    recipies

--- angular-complete/src/app/header/header.component.ts
+++ angular-complete/src/app/header/header.component.ts
@@ -1,9 +1,14 @@
 import {Component} from '@angular/core';
 
 
+
+
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 
-export class HeaderComponent{}
+export class HeaderComponent{
+
+    collapsed = true;
+}
--- angular-complete/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angular-complete/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model'
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is simply a test', "https://static.polityka.pl/_resource/res/path/22/99/229904bf-a6ef-4889-bcab-cd16647d25ed_f1400x900"),
+    new Recipe('A test recipe', 'This is simply a test', "https://static.polityka.pl/_resource/res/path/22/99/229904bf-a6ef-4889-bcab-cd16647d25ed_f1400x900")
+  ];
+
 
   constructor() { }
 
